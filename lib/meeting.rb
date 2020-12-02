62d48e1cef2e14661140e326379baa119c93ae0a
Tim <timcpb@hotmail.com>
2020-12-02 22:14:54 +0000

    refactor

--- lib/meeting.rb
+++ lib/meeting.rb
@@ -1,10 +1,6 @@
 def meeting(s)
-  array = s.split(";")
-  formatted_array = array.map do |name|
-    single_name_array = name.upcase.split(":")
-    p single_name_array
-    "(#{single_name_array.reverse.join(", ")})"
+  formatted_array = s.split(";").map do |name|
+    "(#{name.upcase.split(":").reverse.join(", ")})"
   end
-  p formatted_array
   formatted_array.sort.join()
 end
--- spec/meeting_spec.rb
+++ spec/meeting_spec.rb
@@ -10,4 +10,12 @@ describe "meeting" do
     expect(meeting("Victoria:Schwarz;John:Bell")).to eq("(BELL, JOHN)(SCHWARZ, VICTORIA)")
   end
 
+  it "sorts and formates multiple names, including matching surnames" do
+    expect(meeting("Alexis:Wahl;John:Bell;Victoria:Schwarz;Abba:Dorny;Grace:Meta;Ann:Arno;Madison:STAN;Alex:Cornwell;Lewis:Kern;Megan:Stan;Alex:Korn")).to eq("(ARNO, ANN)(BELL, JOHN)(CORNWELL, ALEX)(DORNY, ABBA)(KERN, LEWIS)(KORN, ALEX)(META, GRACE)(SCHWARZ, VICTORIA)(STAN, MADISON)(STAN, MEGAN)(WAHL, ALEXIS)")
+  end
+
+  it "sorts and formates multiple names, including matching surnames" do
+    expect(meeting("Alexis:Wahl;John:Bell;Victoria:Schwarz;Abba:Dorny;Grace:Meta;Ann:Arno;Madison:STAN;Alex:Cornwell;Lewis:Kern;Megan:Stan;Alex:Korn")).to eq("(ARNO, ANN)(BELL, JOHN)(CORNWELL, ALEX)(DORNY, ABBA)(KERN, LEWIS)(KORN, ALEX)(META, GRACE)(SCHWARZ, VICTORIA)(STAN, MADISON)(STAN, MEGAN)(WAHL, ALEXIS)")
+  end
+
 end
