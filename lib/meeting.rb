af7e6f845a72b76fd17133523fd23600bc05b1ef
Tim <timcpb@hotmail.com>
2020-12-02 00:47:45 +0000

    red test - sorts and formates two names

--- lib/meeting.rb
+++ lib/meeting.rb
@@ -1,8 +1,10 @@
 def meeting(s)
   array = s.split(";")
-  foramtted_array = array.map do |name|
+  formatted_array = array.map do |name|
     single_name_array = name.upcase.split(":")
+    p single_name_array
     "(#{single_name_array.reverse.join(", ")})"
   end
-  foramtted_array.join()
+  p formatted_array
+  formatted_array.join()
 end
