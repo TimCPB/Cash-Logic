7e049e4c91458c471c03c73b6aac02fbd3b89c1e
Tim <timcpb@hotmail.com>
2020-12-15 13:11:15 +0000

    refactor with instance variable

--- lib/cash_converter.rb
+++ lib/cash_converter.rb
@@ -1,9 +1,14 @@
 class Change_generator
 
+  def initialize
+    @change_array = []
+  end
+
   def convert(n)
     if n = 10
-      ["£10"]
+      @change_array.push("£10")
     end
+    @change_array
   end
 
 end
