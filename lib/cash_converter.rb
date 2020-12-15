59ce67857d58a9e835696715f4d2667de60702e2
Tim <timcpb@hotmail.com>
2020-12-15 13:58:01 +0000

    save

--- lib/cash_converter.rb
+++ lib/cash_converter.rb
@@ -1,14 +1,20 @@
 class Change_generator
 
   def initialize
-    @change_array = []
+    # @change_array = []
+    @pounds = [50, 20, 10, 5, 2, 1]
+    @pence = [0.5, 0.2, 0.1, 0.05, 0.02, 0.01]
   end
 
   def convert(n)
-    if n = 10
-      @change_array.push("£10")
+    change_array = []
+    @pounds.each do |amount|
+      if n >= amount
+        change_array.push("£#{amount}")
+        n -= amount
+      end
     end
-    @change_array
+      change_array
   end
 
 end
--- spec/cash_converter_spec.rb
+++ spec/cash_converter_spec.rb
@@ -11,4 +11,26 @@ describe 'change_generator' do
     expect(@cg.convert(10)).to eq(["£10"])
   end
 
+  it "returns any note value for its equivalent integer" do
+    expect(@cg.convert(50)).to eq(["£50"])
+    # expect(@cg.convert(20)).to eq(["£20"])
+    expect(@cg.convert(5)).to eq(["£5"])
+  end
+
+  # it "returns any single coin value" do
+  #   expect(@cg.convert(2)).to eq("£2")
+  #   expect(@cg.convert(1)).to eq("£1")
+  #   expect(@cg.convert(0.50)).to eq("50p")
+  #   expect(@cg.convert(0.20)).to eq("20p")
+  #   expect(@cg.convert(0.10)).to eq("10p")
+  #   expect(@cg.convert(0.05)).to eq("5p")
+  #   expect(@cg.convert(0.02)).to eq("2p")
+  #   expect(@cg.convert(0.01)).to eq("1p")
+  # end
+
+  it "calculates least amount of change for an integer" do
+    expect(@cg.convert(70)).to eq(["£50", "£20"])
+  end
+
+
 end
